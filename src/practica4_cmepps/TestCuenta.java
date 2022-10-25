50445417e585fa6a158f01aaa1ae6b1d794dae8f
antonio diego <antonio diego@DESKTOP-2LM465D>
2022-10-25 10:34:03 +0200

    Pruebas creadas

---
+++ src/practica4_cmepps/TestCuenta.java
@@ -0,0 +1,35 @@
+package practica4_cmepps;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestCuenta {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		cuenta.ingresar();
+		assertEquals(100,cuenta.getSaldo());
+	}
+
+}
